5f10641188b7c611eea4feb4d3bf16fa0d0f97f2
Christine Jiang <56368090+christinejiang11@users.noreply.github.com>
2024-02-19 18:18:35 -0500

    Update responses.sql
    example queries

--- consulting_example/responses.sql
+++ consulting_example/responses.sql
@@ -1,1 +1,30 @@
+--Question 1: Top 5 customers who purchased product category "phones". 
+--Join orders to customers to products to match sales to product category
+--Group by customer name and limit to top 5
+SELECT customer.name as customer_name, 
+    SUM(sales) as sale
+FROM orders
+LEFT JOIN customer
+  ON orders.customer_id = customer.id
+LEFT JOIN product
+  ON product.id = orders.product_id
+WHERE category = "Phones"
+ORDER BY 2 desc
+LIMIT 5;
 
+--Question 2: Top 2 customers who purchased product category "paper". 
+--Join orders to customers to products to match sales to product category
+--Group by customer name and limit to top 5
+SELECT customer.name as customer_name, 
+    SUM(sales) as sale
+FROM orders
+LEFT JOIN customer
+  ON orders.customer_id = customer.id
+LEFT JOIN product
+  ON product.id = orders.product_id
+WHERE category = "Paper"
+ORDER BY 2 desc
+LIMIT 2;
+
+--Rest of queries and brief explanation of logic here
+--[Note for students: queries above are for illustrative purposes only.]
